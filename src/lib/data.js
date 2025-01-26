4d34352cbead4b272218804cd97432391d7fe129
Joshua Green <joshuagreen@Joshuas-MacBook-Pro.local>
2025-01-26 01:01:43 +0000

    updates

--- src/lib/data.js
+++ src/lib/data.js
@@ -4,70 +4,70 @@ export const portfolioItems = [
       title: "People & Planet 2022",
       type: "project",
       image: "/images/image 1.png",
-      content: "Project content here..."
+      content: "While working at Mettle Studio I designed 'People and Planet' - a digital assessment tool that helps civil engineering project managers evaluate community and environmental impacts. Created in partnership with the Environment Agency, Scottish Water and Mott MacDonald, the tool analyses social and environmental indicators to identify potential risks and opportunities."
     },
     {
       id: 2,
       title: "Japan Comics Project 2024",
       type: "project",
       image: "/images/image 2.png",
-      content: "Project content here..."
+      content: "Over nine months in Japan, I created a comic-journalism project documenting Japanese culture and society. Inspired by Joe Sacco's graphic narratives, the work embraced comics as a deliberately slow medium for both creation and consumption, countering today's rapid media landscape."
     },
     {
       id: 3,
       title: "Crumle Magazine 2019",
       type: "project",
       image: "/images/image 3.png",
-      content: "Article content here..."
+      content: "Crumble Magazine, a joint publication of Edinburgh University and Edinburgh College of Art, features writing from students and professionals who aim to energize architectural discourse. I served as an illustrator for several issues."
     },
     {
       id: 4,
       title: "Artichoke Print Studio 2023",
       type: "project",
       image: "/images/image 4.png",
-      content: "Project content here..."
+      content: "At Artichoke Print Studio's open access workshops, I learned printmaking techniques including etching and aquatint. The facility serves artists, students, and community groups with seven presses, including two Rochat Etching Presses, supporting both intaglio and relief printing. The studio offers courses and technical support for all skill levels."
     },
     {
       id: 5,
       title: "Civic Ice Cream 2020",
       type: "project",
       image: "/images/image 5.png",
-      content: "Article content here..."
+      content: "After the pandemic, I created Edinburgh Civic Ice Cream, a community project inspired by La Grotta Ices. The initiative used ice cream making as a starting points for building back the relationships which had been impacted by the pandemic. - participants borrow equipment to create and share their own flavors, fostering social bonds through a simple shared experience."
     },
     {
       id: 6,
       title: "Junction Playbook 2024",
       type: "project",
       image: "/images/image 6.png",
-      content: "Project content here..."
+      content: "The Junction Playbook, developed through the MA Global Collaborative Design program, is a resource guide for food waste initiatives. Working with organisations like Incredible Edible and Father Nature, we created a website and publication that shares creative strategies and enables knowledge exchange between food waste projects."
     },
     {
       id: 7,
       title: "Jura Residency 2019",
       type: "project",
       image: "/images/image 7.png",
-      content: "Article content here..."
+      content: "During a two-week residency in the Jura, I studied baroque instrument craftsmanship, focusing on clavichords and harpsichords. This experience shaped my interest in preserving traditional craft in the face of automation. We are living at a time of rapidly emerging technologies and risk losing our creative industries and professionals if these types of crafts aren't given the attention they deserve"
     },
     {
         id: 8,
         title: "Turning Collective 2022",
         type: "project",
         image: "/images/image 8.png",
-        content: "This Digital Probes Archive serves as an interactive collection of tools designed to illuminate the complex relationship between artificial intelligence and our digital commons. Each probe functions as a hands-on experiment, allowing citizens to directly engage with and understand how AI systems can impact our civic autonomy. From exploring algorithmic bias to examining data ownership, these probes reveal the often invisible ways AI shapes our digital landscape. By making these concepts tangible and explorable, the archive empowers visitors to think critically about AI's role in our society and the importance of protecting our collective digital rights."
+        content: "An hour’s drive from Lyon there is a small village called Cerin. In the early 1980s my grandparents moved here (my grandfather was a furniture maker in Oxford, owning a small studio called Crowdys, and in his retirement brought his workshop to the village). The workshop became a space for learning about traditional crafts such as woodturning on lathes, and also a space for sharing language and stories. In the village, which is also home to an unusually large number of palaeontologists, being located near a famous fossil discovery site, there is now an unusually large number of residents who are skilled on the lathe. The workshop still exists 40 years later and most summers I return to continue the trend of creating, teaching and learning. The community which exists around this one workshop is multi-generational and multilingual - both important parts of how I think about my design practice."
       },
           {
       id: 9,
-      title: "Collaborative Toolkit 2023",
+      title: "SPR Toolkit 2023",
       type: "project",
       image: "/images/image 9.png",
-      content: "Project content here..."
+      content: "I co-developed the Single Parents' Rights Toolkit, a resource that helps HR teams understand and address workplace challenges facing single parents. Based on research with London's single parent community and created in partnership with Single Parents Rights Networks, the toolkit promotes inclusive workplace policies through participatory design methods."
     },
     {
       id: 10,
-      title: "Milking Stools 2022",
+      title: "Cutty Stools 2022",
       type: "project",
       image: "/images/image 10.png",
-      content: "Article content here..."
+      content: "Through crafting traditional Scottish cutty-stools, I explored how furniture can embody cultural history. These three-legged chairs, known for their role in a pivotal 1637 church protest, represent more than just functional objects. By recreating these historically significant pieces, I examined our shifting relationship with crafted objects in an age of mass production, questioning how we might rebuild meaningful connections to the things we make and use."
     },
     {
       id: 11,
@@ -94,6 +94,7 @@ export const portfolioItems = [
       id: 14,
       title: "Data of Everyday Objects 2024",
       type: "project",
+      link: "https://myobjectsgallery.vercel.app/",
       image: "/images/image 14.png",
       content: "Article content here..."
     },
@@ -101,6 +102,7 @@ export const portfolioItems = [
         id: 15,
         title: "Digital Probes Archive 2024",
         type: "project",
+        link: "https://digital-probes-archive-2.vercel.app/",
         image: "/images/image 15.png",
         content: "This Digital Probes Archive serves as an interactive collection of tools designed to illuminate the complex relationship between artificial intelligence and our digital commons. Each probe functions as a hands-on experiment, allowing citizens to directly engage with and understand how AI systems can impact our civic autonomy. From exploring algorithmic bias to examining data ownership, these probes reveal the often invisible ways AI shapes our digital landscape. By making these concepts tangible and explorable, the archive empowers visitors to think critically about AI's role in our society and the importance of protecting our collective digital rights."
       },
@@ -108,6 +110,7 @@ export const portfolioItems = [
         id: 16,
         title: "Quantified Carbon Workshops 2023",
         type: "project",
+        link: "https://www.quantifiedcarbon.com/",
         image: "/images/image 16.png",
         content: "Project content here..."
       },
@@ -166,6 +169,46 @@ export const portfolioItems = [
           type: "project",
           image: "/images/image 24.png",
           content: "Article content here..."
+        },
+        {
+          id: 25,
+          title: "Graphic Narratives: from ‘icon of illiteracy’ to the last bastions of literacy",
+          type: "article",
+          link: "https://drive.google.com/file/d/1Ny89mlm0agol9EXWdq93pRfqzF_-lopR/view?usp=sharing",
+          image: "/images/image 25.png",
+          content: "In our current media age, a proliferation of images and stories of atrocity have made it almost impossible to comprehend the violence that they depict and the suffering experienced by the people in them. This dissertation is an exploration of graphic narrative, and how it can reinstate reader engagement with the pain of others, and how Maus, The Photographer and Palestine, all respond to the moral questions surrounding the representation of conflict."
+        },
+        {
+          id: 26,
+          title: "What Zadie Smith can teach us about smart cities",
+          type: "article",
+          link: "https://drive.google.com/file/d/1X8hdZ1XalkF1sN6nYsAioqVCb98EjFhq/view?usp=sharing",
+          image: "/images/image 26.png",
+          content: "“How urban spaces are experienced can be seen as determined by the competing ideological standpoints of architects, developers and social activists. Despots like Robert Moses and Le Corbusier represent a ‘top-down’ approach towards city planning, where the ‘surgical removal’ of diverse neighborhoods and their replacement with ‘modern slabs’, are seen as the most efficient way of ensuring progress and improvement in a city. In opposition to this ‘homogenizing clarity’, stands the model of ‘bottom-up’ regeneration, advocated by Jane Jacobs, who insists the city is a complex ‘ecosystem’ in which urban spaces are gradually improved through community investment and non-centralized planning (Tyrnauer). The experience of urban life, this essay intends to argue, is normally defined by the equilibrium created by these competing ideological positions; but recently, the development of new technology and the increasing influence of digitization, has favored the ‘top-down’ model of city planning."
+        },
+        {
+          id: 27,
+          title: "Time and Design According to Conrad and Chaplin",
+          type: "article",
+          link: "https://drive.google.com/file/d/1Ny89mlm0agol9EXWdq93pRfqzF_-lopR/view?usp=sharing",
+          image: "/images/image 27.png",
+          content: "In our current media age, a proliferation of images and stories of atrocity have made it almost impossible to comprehend the violence that they depict and the suffering experienced by the people in them. This dissertation is an exploration of graphic narrative, and how it can reinstate reader engagement with the pain of others, and how Maus, The Photographer and Palestine, all respond to the moral questions surrounding the representation of conflict."
+        },
+        {
+          id: 28,
+          title: "The Country Defines the City",
+          type: "article",
+          link: "https://drive.google.com/file/d/1Ny89mlm0agol9EXWdq93pRfqzF_-lopR/view?usp=sharing",
+          image: "/images/image 28.png",
+          content: "In our current media age, a proliferation of images and stories of atrocity have made it almost impossible to comprehend the violence that they depict and the suffering experienced by the people in them. This dissertation is an exploration of graphic narrative, and how it can reinstate reader engagement with the pain of others, and how Maus, The Photographer and Palestine, all respond to the moral questions surrounding the representation of conflict."
+        },
+        {
+          id: 29,
+          title: "What we can learn from Ed Wolfe",
+          type: "article",
+          link: "https://drive.google.com/file/d/1Ny89mlm0agol9EXWdq93pRfqzF_-lopR/view?usp=sharing",
+          image: "/images/image 29.png",
+          content: "In our current media age, a proliferation of images and stories of atrocity have made it almost impossible to comprehend the violence that they depict and the suffering experienced by the people in them. This dissertation is an exploration of graphic narrative, and how it can reinstate reader engagement with the pain of others, and how Maus, The Photographer and Palestine, all respond to the moral questions surrounding the representation of conflict."
         },
   ];
   
